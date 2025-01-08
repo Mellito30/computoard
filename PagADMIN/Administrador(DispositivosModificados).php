7d3ebf0fdbf14a4f9a4b234dc9bbb4e53d23d640
Franddy <franddysandoval7@gmail.com>
2025-01-08 11:59:34 -0800

    Hola commit

--- PagADMIN/Administrador(DispositivosModificados).php
+++ PagADMIN/Administrador(DispositivosModificados).php
@@ -34,7 +34,7 @@ $result = $conn->query($sql);
         </nav class="div-Nav">
             <ul class="Barra-Nav">
                 <li class="div-Sec">
-                        <img class="Logo-ARM" src="img/LOGOARM.png" alt="LOGOARM">
+                        <img class="Logo-ARM" src="img/LOGOARM.png" alt="LOGOAR">
                 </li>
                 <li class="div-Sec">
                     <a class="Link-Nav" href="Index-Administrador.php">Inicio</a><br>
