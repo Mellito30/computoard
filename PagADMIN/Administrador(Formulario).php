7ae6cc390784fd83be1a1978d9682b0934b0e0da
Doratrisssss <doratriz13@gmail.com>
2025-01-16 08:11:26 -0400

    formulario

--- PagADMIN/Administrador(Formulario).php
+++ PagADMIN/Administrador(Formulario).php
@@ -114,55 +114,69 @@ if (isset($_GET['mensaje'])) {
         </div>
     </section>
     
+
+
+
     <!-- Sección de formulario -->
     <section class="Sec-Body1">
-        <form action="" method="POST">
 
-            <div class="div-formulario">
-                <label class="Label-formulario">Estado:</label>
-                <div class="Estados-formulario">
-                    <input class="Input-Formulario" type="radio" id="reparado" name="estado" value="Reparado" required>
-                    <label class="Estado-opciones" for="reparado">Reparado</label><br>
-
-                    <input class="Input-Formulario" type="radio" id="en_proceso" name="estado" value="En Proceso" required>
-                    <label class="Estado-opciones" for="en_proceso">En Proceso</label><br>
-
-                    <input class="Input-Formulario" type="radio" id="sin_iniciar" name="estado" value="Sin Iniciar" required>
-                    <label class="Estado-opciones" for="sin_iniciar">Sin Iniciar</label><br>
-                </div><br>
-            </div>
-
-            <div class="div-formulario">
-                <label class="Label-formulario" for="nombre">Nombre del Departamento:</label>
-                <input class="Input-Formulario" type="text" id="nombre" name="nombre" required><br><br>
-            </div>
-
-            <div class="div-formulario">
-                <label class="Label-formulario" for="tipo">Tipo de dispositivo:</label>
-                <input class="Input-Formulario" type="text" id="tipo" name="tipo" required><br><br>
-            </div>    
-
-            <div class="div-formulario">
-                <label class="Label-formulario" for="modelo">Modelo del dispositivo:</label>
-                <input class="Input-Formulario" type="text" id="modelo" name="marca" required><br><br>
-            </div>
-
-            <div class="div-formulario">
-                <label class="Label-formulario" for="entradafecha">Fecha de Ingreso:</label>
-                <input class="Input-Formulario" type="date" id="entradafecha" name="entradafecha" required><br><br>
-            </div>
-
-            <div class="div-formulario">
-                <label class="Label-formulario" for="salidafecha">Fecha de Salida:</label>
-                <input class="Input-Formulario" type="date" id="salidafecha" name="salidafecha"><br><br>
-            </div>
-
-            <button class="Botton-formulario" type="submit">Agregar</button>
-        </form>
+    <section class="Nav-Revision">
+        <div class="div-Nav-revision">
+            <button>
+                <a href="Administrador(Formulario).php">Ingreso de Dispositivos</a>
+                <a href="Administrador(FormularioModificados).php">Modificación de Dispositivos</a>
+            </button>
+        </div>
     </section>
 
-    <script src="Administrador-(Dispositivos).js"></script>
-    <script src="Administrador-(Cuenta).js"></script>
+    <form action="" method="POST" class="Formulario">
+        <h2 class="Titulo-Formulario">Ingreso de Dispositivos</h2>
+        <div class="div-formulario">
+            <label class="Label-formulario">Estado:</label>
+            <div class="Estados-formulario">
+                <input class="Input-Formulario" type="radio" id="reparado" name="estado" value="Reparado" required>
+                <label class="Estado-opciones" for="reparado">Reparado</label>
+
+                <input class="Input-Formulario" type="radio" id="en_proceso" name="estado" value="En Proceso" required>
+                <label class="Estado-opciones" for="en_proceso">En Proceso</label>
+
+                <input class="Input-Formulario" type="radio" id="sin_iniciar" name="estado" value="Sin Iniciar" required>
+                <label class="Estado-opciones" for="sin_iniciar">Sin Iniciar</label>
+            </div>
+        </div>
+
+        <div class="div-formulario">
+            <label class="Label-formulario" for="nombre">Nombre del Departamento:</label>
+            <input class="Input-Formulario" type="text" id="nombre" name="nombre" placeholder="Ingrese el nombre del departamento" required>
+        </div>
+
+        <div class="div-formulario">
+            <label class="Label-formulario" for="tipo">Tipo de dispositivo:</label>
+            <input class="Input-Formulario" type="text" id="tipo" name="tipo" placeholder="Ingrese el tipo de dispositivo" required>
+        </div>    
+
+        <div class="div-formulario">
+            <label class="Label-formulario" for="modelo">Modelo del dispositivo:</label>
+            <input class="Input-Formulario" type="text" id="modelo" name="marca" placeholder="ingrese el modelo de dispositivo" required>
+        </div>
+
+        <div class="div-formulario">
+            <label class="Label-formulario" for="entradafecha">Fecha de Ingreso:</label>
+            <input class="Input-Formulario" type="date" id="entradafecha" name="entradafecha" required>
+        </div>
+
+        <div class="div-formulario">
+            <label class="Label-formulario" for="salidafecha">Fecha de Salida:</label>
+            <input class="Input-Formulario" type="date" id="salidafecha" name="salidafecha">
+        </div>
+
+        <button class="Botton-formulario" type="submit">Agregar</button>
+    </form>
+</section>
+
+
+    <script src="JavaScript/Administrador-(Formulario).js"></script>
+    <script src="JavaScript/Administrador-(Cuenta).js"></script>
 
 </body>
 </html>
